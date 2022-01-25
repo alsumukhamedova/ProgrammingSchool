142fca26c31272392e6cf54bcc4134a0408de99b
iddemidov1 <iddemidov@eduhse.ru>
2022-01-25 16:27:07 +0300

    test server is working now

--- aggregation_system/server/server/settings.py
+++ aggregation_system/server/server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'web_client'
+    'web_client',
+    'test_server'
 ]
 
 MIDDLEWARE = [
