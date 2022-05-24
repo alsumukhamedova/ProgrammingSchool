fbf00bc83287c01c5ba1768908c7529ec001cf1d
Мухамедова Алсу Фяритовна <afmukhamedova@edu.hse.ru>
2022-05-24 23:55:22 +0300

    fix template rendering

--- aggregation_system/server/web_client/templates/js.js
+++ aggregation_system/server/web_client/templates/js.js
@@ -31,7 +31,7 @@ function login() {
            "user-password": document.querySelector("body > main > section > div.form-group > input").value,
        },
        success: function() {
-           window.location.href='/profile_edit'
+           window.location.href='/tasks'
        },
        error: function() {
            document.getElementById('incorrectPassword').style.visibility = 'visible';
@@ -43,12 +43,15 @@ function register() {
     $.ajax({
        url: "/sign-up/",
        type: "POST",
+        headers: {
+           "X-CSRFToken": document.cookie.match(new RegExp('(^| )csrftoken=([^;]+)'))[2]
+        },
        data: {
-           "user-name": document.querySelector("body > main > section > div:nth-child(2) > input").value,
-           "user-login": document.querySelector("body > main > section > div:nth-child(3) > input").value,
-           "user-password": document.querySelector("body > main > section > div:nth-child(4) > input").value,
-           "teacher-link": document.querySelector("body > main > section > div:nth-child(5) > input").value,
-           "user-type": document.querySelector("body > main > section > div:nth-child(6) > input").value,
+           "user_name": document.querySelector("body > main > section > div:nth-child(2) > input").value,
+           "user_login": document.querySelector("body > main > section > div:nth-child(3) > input").value,
+           "user_mail": document.querySelector("body > main > section > div:nth-child(4) > input").value,
+           "user_password": document.querySelector("body > main > section > div:nth-child(5) > input").value,
+           "user_type": document.querySelector("body > main > section > div:nth-child(6) > input").value,
        },
        success: function() {
            window.location.href='/login'
@@ -79,20 +82,22 @@ function post_solution() {
       }
     });
   // setTimeout(5000)
-  // $.ajax('/request/check/',
-  //   {
-  //     data: {
-  //         user_id : "123",
-  //         task_id : window.location.pathname.split('/')[window.location.pathname.split('/').length - 1],
-  //         program_language : editor.getSession().getMode().$id,
-  //         testing_stage : "1",
-  //     },
-  //     success: function (data,status,xhr) {   // success callback function
-  //        alert('Success ' + data.message);
-  //        var ooa = data;
-  //     },
-  //     error: function (jqXhr, textStatus, errorMessage) { // error callback
-  //       alert('Возникла непредвиденная ошибка. Пожалуйста, попробуйте позднее.');
-  //     }
-  //   });
+
+}
+function tryGetCheckResult() {
+  $.ajax('/request/check/',
+    {
+      headers: {
+          user_id : document.cookie.match(new RegExp('(^| )id=([^;]+)'))[2],
+          task_id : window.location.pathname.split('/')[window.location.pathname.split('/').length - 1],
+      },
+      success: function (data,status,xhr) {   // success callback function
+         alert('Success ' + data.message);
+         var ooa = data;
+      },
+      error: function (jqXhr, textStatus, errorMessage) { // error callback
+          console.log('Error: ' + errorMessage);
+          setTimeout(tryGetCheckResult, 5000);
+      }
+    });
 }
